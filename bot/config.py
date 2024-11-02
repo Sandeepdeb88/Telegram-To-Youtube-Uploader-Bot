8480c74273a8ab5e024458ca269e4d68e3604f82
Sandeepdeb88 <deb480020@gmail.com>
2024-11-02 16:22:54 +0530

    config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7559622544:AAGBm1sVIBiG4B49Y0XnyQkf6jB2O9KtJRQ"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "YouTube_Video_Upload_Bot"
 
-    API_ID = ""
+    API_ID = "23279936"
 
-    API_HASH = ""
+    API_HASH = "b2ace97fe11fd9bb81e82f2578a8ef37"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "http://548367775843-ulv2fkkrq28pqv600v1a26r8a6coommc.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [1392265217]
 
     VIDEO_DESCRIPTION = ""
 
@@ -22,7 +22,7 @@ class Config:
 
     VIDEO_TITLE_PREFIX = ""
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "| Parishram 2025"
     
     DEBUG = bool()
 
